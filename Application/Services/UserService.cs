5d4718070f61656ab73ba90d54ff6f23a81e357e
NguyenChienThang <chienthang014@gmail.com>
2024-05-22 01:07:25 +0700

    Done 70% sign up

--- Application/Services/UserService.cs
+++ Application/Services/UserService.cs
@@ -1,13 +1,71 @@
-﻿using Application.IServices;
+﻿using Application.IRepositories;
+using Application.IServices;
+using Application.ResponseModels;
+using Application.ViewModels.AccountViewModels;
+using AutoMapper;
+using Domain.Enums.AccountEnums;
+using Domain.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Twilio.Types;
 
 namespace Application.Services
 {
     public class UserService : IUserService
     {
+        private readonly IConfiguration _configuration;
+        private readonly SignInManager<Account> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<Account> _userManager;
+        private readonly IMapper _mapper;
+
+        public UserService(IConfiguration configuration,
+            UserManager<Account> userManager,
+            SignInManager<Account> signInManager,
+            RoleManager<IdentityRole> roleManager,
+            IMapper mapper)
+        {
+            _configuration = configuration;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+            _userManager = userManager;
+            _mapper = mapper;
+        }
+
+        public async Task<bool> SignUpAsync(AccountSignUpModel model)
+        {
+            var user = new Account
+            {
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                Birthday = model.BirthDate,
+                Status = "Is Active",
+                UserName = model.AccountEmail,
+                Sex = (int)model.Sex,
+                Email = model.AccountEmail,
+                PhoneNumber = model.AccountPhone,
+                CreateAt = DateTime.Now,
+            };
+            var result = await _userManager.CreateAsync(user, model.AccountPassword);
+            if (result.Succeeded)
+            {
+                if (!await _roleManager.RoleExistsAsync(RoleAccountModel.Customer.ToString()))
+                {
+                    await _roleManager.CreateAsync(new IdentityRole(RoleAccountModel.Customer.ToString()));
+                }
+                if (await _roleManager.RoleExistsAsync(RoleAccountModel.Customer.ToString()))
+                {
+                    await _userManager.AddToRoleAsync(user, RoleAccountModel.Customer.ToString());
+                }
+                return true;
+            }
+            return false;
+        }
     }
 }
---
+++ Infracstructures/Repositories/UserRepository.cs
@@ -0,0 +1,17 @@
+﻿using Application.IRepositories;
+using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infracstructures.Repositories
+{
+    public class UserRepository : GenericRepository<Account>, IUserRepository
+    {
+        public UserRepository(ElderConnectionContext context) : base(context)
+        {
+        }
+    }
+}
