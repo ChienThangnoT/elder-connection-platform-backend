90d64d982eb83de231cd20735b81e577a0584b0a
uyentramle <uyentramle1010@gmail.com>
2024-06-08 14:12:43 +0700

    update: elderInformation

---
+++ Application/Services/ElderInformationService.cs
@@ -0,0 +1,22 @@
+﻿using Application.IServices;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+	public class ElderInformationService : IElderInformationService
+	{
+		private readonly IUnitOfWork _unitOfWork;
+		private readonly IMapper _mapper;
+
+		public ElderInformationService(IUnitOfWork unitOfWork, IMapper mapper)
+		{
+			_unitOfWork = unitOfWork;
+			_mapper = mapper;
+		}
+	}
+}
---
+++ Infracstructures/Repositories/ElderInformationRepository.cs
@@ -0,0 +1,19 @@
+﻿using Application.IRepositories;
+using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infracstructures.Repositories
+{
+	public class ElderInformationRepository : GenericRepository<ElderInformation>, IElderInformationRepository
+	{
+		private readonly ElderConnectionContext _context;
+		public ElderInformationRepository(ElderConnectionContext context) : base(context)
+		{
+			_context = context;
+		}
+	}
+}
